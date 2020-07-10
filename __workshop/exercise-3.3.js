7aecd8667d028838c8a269ff28934c1a5abf5fd1
Matcote <matthewcotesipod@gmail.com>
2020-07-10 17:26:58 -0400

    3.3 (75%)

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -1,18 +1,19 @@
 // Exercise 3.3 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: "e2c57ffd46ce4b39b729d2794f610b2b",
+    q: `${lat}+${lng}`,
   };
 
   // return ...
+  return opencage.geocode(requestObj).then((data) => data.results[0].formatted);
 }
 
-getAddressFromPosition('48.8584', '2.2945').then((response) =>
+getAddressFromPosition("48.8584", "2.2945").then((response) =>
   console.log(response)
 );
