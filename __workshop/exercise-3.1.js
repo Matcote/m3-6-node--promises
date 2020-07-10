ab6f7d910ff1f948a99f723b99d23bb292e396b6
Matcote <matthewcotesipod@gmail.com>
2020-07-10 16:23:13 -0400

    3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,18 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require("request-promise");
+const { get } = require("request-promise");
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request("http://api.open-notify.org/iss-now.json")
+    .then((response) => JSON.parse(response))
+    .then((location) => location.iss_position)
+    .then((final) => `lat: ${final.latitude} lng: ${final.longitude}`)
+    .catch((err) => {
+      return err.error ? JSON.parse(err.error) : err;
+    });
 };
+
+getIssPosition().then((result) => console.log(result));
