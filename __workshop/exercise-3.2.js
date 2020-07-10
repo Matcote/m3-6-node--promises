fd2f99ad7dea976d4a22879b3312cda7b232cdf2
Matcote <matthewcotesipod@gmail.com>
2020-07-10 17:18:10 -0400

    3.2

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,24 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: "e2c57ffd46ce4b39b729d2794f610b2b",
+    q: address,
   };
 
   // return something...
+  return opencage
+    .geocode(requestObj)
+    .then((data) => data.results[0].annotations.DMS);
 };
 
 getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
+  "1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8"
+).then((response) => console.log(response));
+getPositionFromAddress(
+  "5200 Avenue Rosedale, Montréal, QC H4V 2H6"
 ).then((response) => console.log(response));
