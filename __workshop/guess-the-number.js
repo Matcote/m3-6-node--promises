2a4da48f8c8634386bf55b5ee778af1014313067
Matcote <matthewcotesipod@gmail.com>
2020-07-11 14:21:39 -0400

    stretch goal done

--- __workshop/guess-the-number.js
+++ __workshop/guess-the-number.js
@@ -3,12 +3,38 @@ const request = require("request");
 const inquirer = require("inquirer");
 
 const hiddenNum = Math.round(Math.random() * 100);
+let guesses = 5;
+console.log(hiddenNum);
 inquirer
-  .prompt(["What do you guess?"])
+  .prompt([
+    {
+      type: "input",
+      name: "guess",
+      message: "What's the magic number? (between 0-100)",
+      validate: (value) => {
+        if (value == hiddenNum) {
+          return true;
+        }
+        guesses--;
+        if (guesses === 0) {
+          return true;
+        }
+        if (value > hiddenNum) {
+          return `Too high! ${guesses} guesses remaining!`;
+        } else if (value < hiddenNum) {
+          return `Too low! ${guesses} guesses remaining!`;
+        }
+        return `Wrong! ${guesses} guesses remaining!`;
+      },
+    },
+  ])
   .then((answer) => {
-    if (answer === hiddenNum) {
-      console.log("success");
+    if (answer.guess == hiddenNum) {
+      console.log("Correct! " + answer.guess + " was the hidden number!");
     } else {
+      console.log(
+        "Better luck next time... :/ The magic number was " + hiddenNum
+      );
     }
   })
   .catch((err) => {
